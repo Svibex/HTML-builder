2ffe76eddb8aecba7cf882709a387799f6c277d4
Svibex <future.science@yandex.ru>
2022-05-23 15:25:24 +0300

    feat: complete 4 task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,43 @@
+const fs = require('fs');
+const path = require('path');
 
+const folder = path.join(__dirname, 'files');
+const folderCopy = path.join(__dirname, 'files-copy');
+
+function copyDir() {
+
+  fs.open(folderCopy, err => {
+    if (err) {
+      fs.mkdir(folderCopy, err => {
+        if (err) throw err;
+      });
+    }
+
+    fs.readdir(folderCopy, (err, files) => {
+      if (err) throw err;
+
+      for (const file of files) {
+        fs.unlink(path.join(folderCopy, file), err => {
+          if (err) throw err;
+        });
+      }
+    });
+
+    fs.readdir(
+      folder,
+      (err, files) => {
+        if (err) throw err;
+
+        files.forEach(file => {
+          fs.copyFile(
+            path.join(folder, file),
+            path.join(folderCopy, file),
+            err => {
+              if (err) throw err;
+            });
+        });
+      });
+  });
+}
+
+copyDir();
